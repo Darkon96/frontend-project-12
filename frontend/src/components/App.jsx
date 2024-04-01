9af4b6d55b958bfddafe5df350fd41c956c3967f
Darkon96 <steponframe2@gmail.com>
2024-04-01 11:57:59 +0300

    routing and loginPage

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,3 +1,21 @@
-const App = () => <h1>Hexlet Chat</h1>;
+import ErrorPage from "./NotFoundPage.jsx";
+import Login from "./LoginPage.jsx"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+    return (
+    <div className="d-flex flex-column h-100">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+    );
+  }
 
 export default App;
+
+
