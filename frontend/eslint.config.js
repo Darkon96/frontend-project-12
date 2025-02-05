106896b4d237b2652b6c49f0c9e22e9e987c894d
Darkon96 <steponframe2@gmail.com>
2025-02-05 22:21:02 +0300

    re dependencies

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-=======
 import js from '@eslint/js';
 import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
->>>>>>> parent of 6543d03 (del)
 
 export default [
   { ignores: ['dist'] },
@@ -43,8 +35,4 @@ export default [
       ],
     },
   },
-<<<<<<< HEAD
-]
-=======
 ];
->>>>>>> parent of 6543d03 (del)
